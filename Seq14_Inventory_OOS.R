002552eb900b5160d858f0eea9efcc3c661d6ce1
IT <it@mishon.ca>
2023-04-06 15:35:55 -0400

    Update Seq14_Inventory_OOS.R
    added changes requested as per SDT-49

--- Seq14_Inventory_OOS.R
+++ Seq14_Inventory_OOS.R
@@ -41,13 +41,15 @@ update_inventory_oos <- function()
   {
     prdct_template <- tryCatch(
       {
+        
+        ###Update the condition
         prdct_template <- dbGetQuery(rds_odoodb,"select template.default_code as SKU, supplierinfo.min_qty, supplierinfo.sequence, 
-                            supplierinfo.delay
+                            supplierinfo.delay,template.x_available_in_chesterfield as cf_avail,template.available_in_new_dundee as nd_avail,
+                            available_in_ga as ga_avail 
                              from product_template as template inner join product_supplierinfo as supplierinfo on 
                              supplierinfo.product_tmpl_id=template.id
                              where (template.default_code like 'C-%' or template.default_code like 'K-%') and 
-                             template.active = TRUE and template.product_status='active' and template.available_in_new_dundee = TRUE and 
-                             template.x_available_in_chesterfield = TRUE")
+                             template.active = TRUE and template.product_status='active'")
         
       },
       error=function(e)
@@ -78,7 +80,8 @@ update_inventory_oos <- function()
     prdct_Packaging <- tryCatch(
       {
         prdct_Packaging <- dbGetQuery(rds_odoodb,"select packaging.id,product_template.default_code,
-packaging.sequence, packaging.height, packaging.width, packaging.length, packaging.is_individual, 
+packaging.sequence, packaging.height, packaging.width, packaging.length,packaging.weight,packaging.max_weight,
+packaging.is_individual, 
 packaging.qty  from product_packaging as packaging inner join product_template on 
                            packaging.product_tmpl_id=product_template.id")
         
@@ -107,7 +110,8 @@ packaging.qty  from product_packaging as packaging inner join product_template o
         thisYear <- as.numeric(format(Sys.Date(),"%Y"))
         lastYear <-  thisYear-1
         salesData <- dbGetQuery(rds_mishondb_con,glue("SELECT SKU, SUM(CASE WHEN DATE(DATE) BETWEEN '{lastYear}-01-01' AND '{lastYear}-12-31' THEN QtySold END) AS QtySold_lastYear,
-        SUM(CASE WHEN DATE(DATE) BETWEEN '{thisYear}-01-01' AND '{Sys.Date()}' THEN QtySold END) AS QtySold_YTD 
+        SUM(CASE WHEN DATE(DATE) BETWEEN '{thisYear}-01-01' AND '{Sys.Date()}' THEN QtySold END) AS QtySold_YTD,
+        SUM(CASE WHEN DATE(DATE) BETWEEN '2020-01-01' AND '{Sys.Date()}' THEN QtySold END) AS QtySold_LifeTime 
 FROM dailysales WHERE GMV > 0 GROUP BY SKU"))
         
       },
@@ -118,6 +122,24 @@ FROM dailysales WHERE GMV > 0 GROUP BY SKU"))
     forecastData <- tryCatch(
       {
         forecastData <- dbGetQuery(rds_mishondb_con,"select * from demand_FC_Pur_SKU_odc")
+      },
+      error=function(e)
+      {
+        return(NULL)
+      })
+    
+    forecast_StatusData <- tryCatch(
+      {
+        forecast_StatusData <- dbGetQuery(rds_mishondb_con,"select * from demand_Forecast_status_odc")
+      },
+      error=function(e)
+      {
+        return(NULL)
+      })
+    
+    firstReceived <- tryCatch(
+      {
+        firstReceived <- dbGetQuery(rds_mishondb_con,"select sku,DATE as first_received_date from firstReceived")
       },
       error=function(e)
       {
@@ -138,13 +160,15 @@ FROM dailysales WHERE GMV > 0 GROUP BY SKU"))
       salesData =  salesData[!grepl("KOOZIE|8TEN",toupper(salesData$SKU)),]
       productInventory =  productInventory[!grepl("KOOZIE|8TEN",toupper(productInventory$Product)),]
       
-      stock_Move2 <- stock_Move %>%group_by(sku,date)%>%summarise(order_qty = sum(product_qty),booking_reference=booking_reference[1])%>%slice(which.min(as.Date(date)))
+      stock_Move2 <- stock_Move%>%filter(!grepl("cancel",tolower(booking_reference))) %>%group_by(sku,date)%>%summarise(order_qty = sum(product_qty),booking_reference=booking_reference[1])%>%slice(which.min(as.Date(date)))
       
       prdct_template1 <- prdct_template %>% group_by(sku)%>%slice(which.min(sequence))
       
       validSalesData <- left_join(productInventory,salesData,by=c("Product"="SKU"))
       validSalesData[is.na(validSalesData)] <- 0
       
+      
+      #Qty Sold Calculation
       compDF3 <- data.frame()
       for(i in 1:dim(validSalesData)[1])
       {
@@ -160,6 +184,7 @@ FROM dailysales WHERE GMV > 0 GROUP BY SKU"))
         
         compDF1$QtySold_lastYear <- compDF1$req_quant * req_product$QtySold_lastYear
         compDF1$QtySold_YTD <- compDF1$req_quant * req_product$QtySold_YTD
+        compDF1$QtySold_LifeTime <- compDF1$req_quant * req_product$QtySold_LifeTime
         
         compDF3<- rbind(compDF1 %>% select(-c("Product","req_quant")),compDF3)
         
@@ -168,19 +193,19 @@ FROM dailysales WHERE GMV > 0 GROUP BY SKU"))
       }
      
       summ_SalesDF <- compDF3 %>% group_by(SKU) %>%summarise(QtySold_YTD = sum(QtySold_YTD),
-                                                             QtySold_lastYear  = sum(QtySold_lastYear))
+                                                             QtySold_lastYear  = sum(QtySold_lastYear),
+                                                             QtySold_LifeTime = sum(QtySold_LifeTime))
       
       
       forecastData_Summ <- forecastData %>% filter(product_status !="discontinued") %>% group_by(SKU) %>% summarise(Forecasted_Annual_Qty = sum(FC_QTY))
       
       #
       productData1 <- productInventory[productInventory$Component_Type=="MIS" & !grepl("KOOZIE|8TEN",productInventory$Product),]
-      # productData1 <- productInventory[productInventory$Component_Type=="MIS" & productInventory$Product!="15MIS_KOOZIE1",]
       productData1$refSKU = str_extract_all(productData1$Product,pattern="\\w{1}\\-\\w{3}\\-\\d{4}")%>%unlist()
       
       # #
       
-      join1 <- left_join(prdct_template1%>%select(sku,min_qty,delivery_lead_time=delay),summ_SalesDF,by=c("sku"="SKU"))
+      join1 <- left_join(prdct_template1%>%select(sku,min_qty,delivery_lead_time=delay,cf_avail,nd_avail,ga_avail),summ_SalesDF,by=c("sku"="SKU"))
       join2 <- left_join(join1,forecastData_Summ,by=c("sku"="SKU"))
       join3 <- left_join(join2,productData1%>%select(CHES_on_hand,ND_on_hand,GA_on_hand,refSKU),by=c("sku"="refSKU"))
       join3_1 <- join3[!is.na(join3$sku),]
@@ -226,10 +251,10 @@ FROM dailysales WHERE GMV > 0 GROUP BY SKU"))
         qty_req_after_oos_before_recv = numeric(0)
         
       )
-      # browser()
+     
       for(s in 1:dim(req_product)[1])
       {
-        print(s)
+        # print(s)
         prdctInfo =  req_product[s,]
         
         dateSeq_forecast <- seq(Sys.Date(),Sys.Date()+364,1)
@@ -248,9 +273,7 @@ FROM dailysales WHERE GMV > 0 GROUP BY SKU"))
         
         for(s2 in 1:365)
         {
-          # 
-          # seasonalityinfo = sec_seasonality[sec_seasonality$Date == dateFormatted[s2],]
-          
+         
           seasonality_sum = sum(sec_seasonality_2$sIndex[sec_seasonality_2$id<=s2])
           
           forecastInfo = (seasonality_sum * prdctInfo$Forecasted_Annual_Qty)/365
@@ -266,7 +289,7 @@ FROM dailysales WHERE GMV > 0 GROUP BY SKU"))
             
             if(remaining_Days>0)
             {
-              seasonality_sum2 = sum(sec_seasonality_2$sIndex[sec_seasonality_2$id>s2 & sec_seasonality_2$id <= remaining_Days +30])
+              seasonality_sum2 = sum(sec_seasonality_2$sIndex[sec_seasonality_2$id>s2 & sec_seasonality_2$id <= s2+remaining_Days +30])
               
               forecastInfo2 = (seasonality_sum2 * prdctInfo$Forecasted_Annual_Qty)/365
             } 
@@ -291,6 +314,7 @@ FROM dailysales WHERE GMV > 0 GROUP BY SKU"))
         
       }
       # #
+      #
       #
       join4 <- left_join(join4_1,forecastData_m,by=c("sku"="SKU"))
       
@@ -339,10 +363,17 @@ FROM dailysales WHERE GMV > 0 GROUP BY SKU"))
       
       oosDates3 <- oosDates2[oosDates2$refSKUs %in% oos_Skus, ]
       
-      oosDates3$Days_Untill_OOS <- as.numeric(as.Date(oosDates3$DATE) - Sys.Date())
+      oosDates3$Days_Untill_OOS2 <- as.numeric(as.Date(oosDates3$DATE) - Sys.Date())
       
-      join4$Days_Untill_OOS[join4$sku %in% oosDates3$refSKUs] <-  oosDates3$Days_Untill_OOS
-     
+      #
+      
+      join4 <- left_join(join4,oosDates3%>%select(refSKUs ,Days_Untill_OOS2),by=c("sku"="refSKUs"))
+      
+      join4$Days_Untill_OOS <- coalesce(join4$Days_Untill_OOS,join4$Days_Untill_OOS2)
+      
+      #Added to make sure 
+      join4$Days_Untill_OOS[join4$QtySold_LifeTime>0 & join4$Forecasted_Annual_Qty==0] = 0
+   
       join4$Days_Untill_OOS[is.na(join4$Days_Untill_OOS)] <- 999
       
       join4$OOS_Date <- as.character(Sys.Date() + as.numeric(join4$Days_Untill_OOS))
@@ -360,17 +391,20 @@ FROM dailysales WHERE GMV > 0 GROUP BY SKU"))
       
       
       join4$Risk_Status <- ifelse(join4$booking_reference=="Not on order",
-                                  ifelse((join4$Days_Untill_OOS > 0 & join4$delivery_lead_time > join4$Days_Untill_OOS) | join4$Days_Untill_OOS <= 0 ,"At risk but not on order","Not at risk"),
-                                  ifelse((join4$Days_Untill_OOS > 0 & join4$OOS_before_Receiving =="TRUE"  & join4$delivery_lead_time > join4$Days_Untill_OOS) | join4$Days_Untill_OOS <= 0  ,"At risk on order","Not at risk"))
+                                  ifelse((join4$Days_Untill_OOS > 0 & join4$delivery_lead_time > join4$Days_Untill_OOS) | join4$Days_Untill_OOS < 0 ,"At risk but not on order","Not at risk"),
+                                  ifelse((join4$Days_Untill_OOS > 0 & join4$OOS_before_Receiving =="TRUE"  & join4$delivery_lead_time > join4$Days_Untill_OOS) | join4$Days_Untill_OOS < 0  ,"At risk on order","Not at risk"))
       
+      
+      
+      join4$Risk_Status[join4$QtySold_LifeTime>0 & join4$Forecasted_Annual_Qty==0] = "Unable to identify because of Zero forecast"
       
       
       join4$Product_Status <- ifelse(join4$Total_on_hand ==0 & (join4$Forecasted_Annual_Qty + join4$QtySold_lastYear + join4$QtySold_YTD>0 ),"Out of stock",
                                      ifelse(join4$Total_on_hand ==0& (join4$Forecasted_Annual_Qty + join4$QtySold_lastYear + join4$QtySold_YTD==0), "New Product","In Stock"))
       
+      # browser()
       
-      
-      fill_qty_req <- join4[join4$Product_Status=="Out of stock" & join4$booking_reference!="Not on order" & join4$Forecasted_Annual_Qty>0,]
+      fill_qty_req <- join4[join4$Days_between_oos_receiving>0 & join4$qty_req_after_oos_before_recv==0 & join4$Forecasted_Annual_Qty>0,]
       #
       rds_mishondb_con <- tryCatch(
         {
@@ -425,50 +459,79 @@ FROM dailysales WHERE GMV > 0 GROUP BY SKU"))
           forecast= round(forecastInfo,2)
         )
       }
-      
+      #
       # #
       
       join4$qty_req_after_oos_before_recv[join4$sku %in% filledDF$sku]<- filledDF$forecast
       
-      # #
-      #
-      
-      prdct_Packaging2 = prdct_Packaging%>%
-        filter(is_individual)%>%
-        group_by(default_code)%>%
-        slice(which.min(sequence))%>%select(sku=default_code,height,width,length)
       
       prdct_Packaging3 = prdct_Packaging%>%
         filter(!is_individual)%>%
         group_by(default_code)%>%
-        slice(which.min(sequence))%>%select(sku=default_code,Master_Carton_Qty=qty)
+        slice(which.min(sequence))%>%select(sku=default_code,height,width,length,weight=max_weight,Master_Carton_Qty=qty)
+
       
-      prd_packDF <- left_join(prdct_Packaging2,prdct_Packaging3,by="sku")
+      join4_0_1 <- left_join(join4,prdct_Packaging3,by="sku")
       
-      join4_0_1 <- left_join(join4,prd_packDF,by="sku")
       
-      join4_0_1$height <- join4_0_1$height * 0.08333
-      join4_0_1$width <- join4_0_1$width * 0.08333
-      join4_0_1$length <- join4_0_1$length * 0.08333
-      
-      join4_0_2 <- left_join(join4_0_1,stock_Move%>%select(sku,forecasted_delivery_date),by="sku")
+      join4_0_2 <- left_join(join4_0_1,
+                             stock_Move%>%group_by(sku)%>%
+                               slice(which.min(as.Date(forecasted_delivery_date)))%>%
+                               select(sku,forecasted_delivery_date),by="sku")%>%
+        left_join(.,forecast_StatusData%>%select(-c(FC_QTY,LastUpdatedDate)),by=c("sku"="SKU"))%>%
+        left_join(.,firstReceived,by="sku")
       
       
       join4_0_2$Last_Updated_Date <- Sys.Date()
       join4_0_2$booking_reference = stringi::stri_enc_toascii(as.character(join4_0_2$booking_reference))
       
-      # #
       
-      # stock_Move$delivery_Date = ifelse(is.na(stock_Move$conf_del_date),as.character(stock_Move$org_del_date),as.character(stock_Move$conf_del_date))
-      # 
-      # 
-      # stock_Move123 = stock_Move%>%select(sku,destination,delivery_Date)%>%spread(destination,delivery_Date,-sku)
-      # 
-      # 
-      # 
-      # join5 <- left_join(join4,stock_Move%>%select(sku,delivery_Date),by="sku")
+      finalResultDF <- join4_0_2%>%select(
+        sku,
+        product_status,
+        purchase_ok,
+        as.character(cf_avail),
+        as.character(nd_avail),
+        as.character(ga_avail),
+        CHES_on_hand,
+        ND_on_hand,
+        GA_on_hand,
+        Total_on_hand,
+        first_received_date,
+        QtySold_YTD,
+        QtySold_lastYear,
+        QtySold_LifeTime,
+        Forecasted_Annual_Qty,
+        forecast_status,
+        delivery_lead_time,
+        min_order_qty = min_qty,
+        Next_receipt_Date,
+        forecasted_delivery_date,
+        booking_reference,
+        On_Order_Qty,
+        OOS_Date,
+        Days_Untill_OOS,
+        OOS_before_Receiving,
+        Days_between_oos_receiving,
+        qty_req_after_oos_before_recv,
+        Risk_Status,
+        width_Inch = width,
+        height_Inch = height,
+        length_Inch = length,
+        weight = weight,
+        Master_Carton_Qty,
+        Last_Updated_Date
+        
+        
+        
+        
+        
+      )
       
-      # #
+      
+      
+      
+      #
       
       rds_mishondb_con <- tryCatch(
         {
@@ -485,13 +548,16 @@ FROM dailysales WHERE GMV > 0 GROUP BY SKU"))
           return(NULL)
         })
       
-      # browser()
+      # #
       
       #
-      if(!is.null(rds_mishondb_con) & dim(join4_0_2)[1]>0)
+      if(!is.null(rds_mishondb_con) & dim(finalResultDF)[1]>0)
       {
-        DBI::dbGetQuery(rds_mishondb_con, "TRUNCATE TABLE oos_inventory_report")
-        DBI::dbWriteTable(rds_mishondb_con,"oos_inventory_report",join4_0_2,row.names=FALSE,append=TRUE)
+        if(DBI::dbExistsTable(rds_mishondb_con,"oos_inventory_report"))
+        {
+          DBI::dbSendQuery(rds_mishondb_con, "TRUNCATE TABLE oos_inventory_report")
+        }
+        DBI::dbWriteTable(rds_mishondb_con,"oos_inventory_report",finalResultDF,row.names=FALSE,append=TRUE)
         
         dbDisconnect(conn=rds_mishondb_con)
         
